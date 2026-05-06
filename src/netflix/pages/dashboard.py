0bd2085b28d1b656a11a7d860d9d9af1d55c3f2a
giraudc - cpu <giraudcgc@gmail.com>
2026-05-06 15:45:21 +0200

    feat: Added metrics/kpis in dashboard.py and with function in  helpers.py

--- src/netflix/pages/dashboard.py
+++ src/netflix/pages/dashboard.py
@@ -1,8 +1,25 @@
 import streamlit as st
 from netflix.components.visuals import russia_line_chart
 from netflix.utils.helpers import show_banner
+from netflix.utils.helpers import get_russia_kpi, get_country_df
+
+
 
 show_banner()
+
+st.divider()
+
+
+st.subheader("A data anomaly that reveals a geopolitical event.")
+
+st.markdown("""
+    <p class='disclaimer'>
+        This dashboard explores Netflix viewing data up to February 2022 only visible after exploratory data analysis (EDA). <br>
+        The chart line and metrics below show what Netflix looked like in Russia until the events.
+    </p>
+""", unsafe_allow_html=True)
+
+st.divider()
 
 
 st.markdown(
@@ -13,3 +30,50 @@ st.markdown(
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     russia_line_chart()
+
+
+# --- Trying to separate this part from the onlione chart ---
+st.divider()
+
+# Values in the center of the cards
+st.markdown("""
+    <style>
+        [data-testid="stMetric"] {
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        [data-testid="stMetricValue"] {
+            justify-content: center;
+            text-align: center;
+        }
+        [data-testid="stMetricLabel"] {
+            text-align: center;
+            width: 100%;
+        }
+        [data-testid="stMetricDelta"] {
+            justify-content: center;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+
+# Getting all the calculated numbers
+kpi = get_russia_kpi()  
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.metric(label="Unique shows at the start (Jul 2021)", value=kpi["start_2021"])
+
+with col2:
+    st.metric(label="Unique shows end 2021", value=kpi["end_2021"],
+        delta=f"{kpi['diff_2021']:+d} ({kpi['pct_2021']:+.1f}%)")
+
+with col3:
+    st.metric(label="Peak in Jan 2022", value=kpi["start_2022"])
+
+with col4:
+    st.metric(label="Last data from Feb 2022", value=kpi["end_2022"],
+        delta=f"{kpi['both_years']:+.0f} ({kpi['both_years_pct']:+.1f}%) vs Jul 2021")
+
--- src/netflix/utils/helpers.py
+++ src/netflix/utils/helpers.py
@@ -58,3 +58,58 @@ def show_banner():
     from netflix.utils.constants import IMAGE_PATH
     st.image(str(IMAGE_PATH / "Logga_Streamly.png"), width=200)
     st.caption("Global Netflix viewing statistics")
+
+
+
+# --- For Russias KPIs using unique "shows" (film and series) ---
+@st.cache_data
+def get_russia_kpi():
+
+# Loading the data and filtering only Russia
+    df = get_country_df()
+    df["week"] = pd.to_datetime(df["week"])
+    russia = df[df["country_name"] == "Russia"]
+
+# Counting unique shows at beginning and ends of 2021 and 2022 (march)
+# Filtering now month (July) and extracting unique value
+    start_2021 = russia[russia["month"] == 7]["show_title"].nunique()
+    end_2021   = russia[russia["month"] == 12]["show_title"].nunique()
+    start_2022 = russia[(russia["year"] == 2022) & (russia["month"] == 1)]["show_title"].nunique()
+    end_2022   = russia[(russia["year"] == 2022) & (russia["month"] == 2)]["show_title"].nunique()
+
+# Calculating the difference | function only used here
+    def cal_diff(start, end):
+        return end - start, ((end - start) / start) * 100
+    
+    diff_2021, prct_2021 = cal_diff(start_2021, end_2021)
+    diff_2022, prct_2022 = cal_diff(start_2022, end_2022)
+    both_years, both_years_prct  = cal_diff(start_2021, end_2022)
+
+# returning a dict
+    return {
+    "start_2021":  start_2021,
+    "end_2021":    end_2021,
+    "diff_2021": diff_2021,
+    "pct_2021":    prct_2021,
+    "start_2022":  start_2022,
+    "end_2022":    end_2022,
+    "diff_2022": diff_2022,
+    "pct_2022":    prct_2022,
+    "both_years":     both_years_prct,
+    "both_years_pct": both_years_prct,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
