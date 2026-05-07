47a53057d10c1f96a381e10945f90718db704cce
mandcoder <anderssonmaarcus@icloud.com>
2026-05-07 07:51:55 +0200

    Compare feature (#14) - Reviewed by Aira
    * add folder structure with gitkeep
    
    * local changes to insights.py
    
    * add compare function without KPI-cards
    
    * Added KPI-card to compare function
    
    ---------
    
    Co-authored-by: maarcus_andersson <marcus.andersson@stud.sti.se>

--- src/netflix/pages/insights.py
+++ src/netflix/pages/insights.py
@@ -3,6 +3,7 @@ from netflix.utils.helpers import get_global_df, get_metadata_df
 from netflix.utils.helpers import read_css
 from netflix.utils.constants import STYLES_PATH
 
+
 read_css(STYLES_PATH / "insights.css")
 
 df_global = get_global_df()
@@ -14,7 +15,6 @@ st.divider()
 
 # Dropdowns
 all_titles = sorted(df_global["show_title"].unique())
-
 col_left, col_right = st.columns(2, gap="large", vertical_alignment="top")
 
 with col_left:
@@ -50,11 +50,16 @@ def show_info(title, meta):
     st.subheader(title.title())
     if meta is not None:
         st.caption(f"⭐ {meta['rating']} / 10")
-        st.write(
-            meta["description"]
+
+        description = (
+            str(meta["description"])
             if str(meta["description"]) != "nan"
             else "Data is not available"
         )
+        if len(description) > 150:
+            description = description[:150] + "..."
+        st.write(description)
+
         st.caption(
             f"**Cast:** {meta['cast_members'] if str(meta['cast_members']) != 'nan' else 'Data is not available'}"
         )
@@ -71,14 +76,48 @@ def get_metadata(title):
     return match.iloc[0] if not match.empty else None
 
 
+def show_kpi(stats):
+    """Visar KPI-kort med statistik annars felmeddelande"""
+    if stats is not None:
+        st.divider()
+        k1, k2, k3 = st.columns(3)
+        k1.metric("Weeks in Global Top 10", stats["global_weeks_in_top10"], border=True)
+        k2.metric("Best Global Rank Achieved", stats["global_best_rank"], border=True)
+        k3.metric(
+            "Average Global Rank in Top 10", stats["global_avg_rank"], border=True
+        )
+    else:
+        st.warning("Data is missing")
+
+
+def show_title_card(col, title, meta, stats):
+    """Samlar alla delar i ett kort, anropar show_posterm show_info och show_kpi"""
+
+    with col:
+        show_poster(meta)
+        show_info(title, meta)
+        show_kpi(stats)
+
+
+def get_stats(title):
+    """Hämtar statistik för den givna titeln från FactGlobal_Final.csv"""
+    data = df_global[df_global["show_title"] == title]
+    if data.empty:
+        return None
+    return {
+        "global_weeks_in_top10": int(data["cumulative_weeks_in_top_10"].max()),
+        "global_best_rank": int(data["weekly_rank"].min()),
+        "global_avg_rank": round(data["weekly_rank"].mean()),
+    }
+
+
 if title_left:
     meta_left = get_metadata(title_left)
-    with col_left:
-        show_poster(meta_left)
-        show_info(title_left, meta_left)
+    stats_left = get_stats(title_left)
+    show_title_card(col_left, title_left, meta_left, stats_left)
+
 
 if title_right:
     meta_right = get_metadata(title_right)
-    with col_right:
-        show_poster(meta_right)
-        show_info(title_right, meta_right)
+    stats_right = get_stats(title_right)
+    show_title_card(col_right, title_right, meta_right, stats_right)
